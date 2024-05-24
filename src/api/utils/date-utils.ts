0c150a2fd1e54bc566bf2c5270cbd23dc4e842b1
Michael Johnson <michael@icefoganalytics.com>
2024-05-24 14:40:52 -0700

    Fixing date issues

--- src/api/utils/date-utils.ts
+++ src/api/utils/date-utils.ts
@@ -4,11 +4,25 @@ import moment from "moment";
 export function weeksBetween(start: Date | string | undefined, end: Date | string | undefined): number {
   if (isUndefined(start) || isUndefined(end)) return 0;
 
-  return Math.ceil(moment.utc(end).endOf("week").diff(moment.utc(start).startOf("week"), "week", true));
+  const weeks = Math.ceil(moment.utc(end).endOf("week").diff(moment.utc(start).startOf("week"), "week", true));
+
+  if (weeks == 53) {
+    const days = Math.ceil(moment.utc(end).diff(moment.utc(start), "days", true));
+    if (days <= 366) return 52;
+  }
+
+  return weeks;
 }
 
 export function monthsBetween(start: Date | string | undefined, end: Date | string | undefined): number {
   if (isUndefined(start) || isUndefined(end)) return 0;
 
-  return Math.ceil(moment.utc(end).endOf("month").diff(moment.utc(start).startOf("month"), "month", true));
+  const months = Math.ceil(moment.utc(end).endOf("month").diff(moment.utc(start).startOf("month"), "month", true));
+
+  if (months == 13) {
+    const days = Math.ceil(moment.utc(end).diff(moment.utc(start), "days", true));
+    if (days <= 366) return 12;
+  }
+
+  return months;
 }
--- src/web/src/components/application/assessments/store/utils.js
+++ src/web/src/components/application/assessments/store/utils.js
@@ -4,21 +4,35 @@ import moment from "moment";
 export function weeksBetween(start, end) {
   if (isUndefined(start) || isUndefined(end)) return 0;
 
-  return Math.ceil(
+  const weeks = Math.ceil(
     moment
       .utc(end)
       .endOf("week")
       .diff(moment.utc(start).startOf("week"), "week", true)
   );
+
+  if (weeks == 53) {
+    const days = Math.ceil(moment.utc(end).diff(moment.utc(start), "days", true));
+    if (days <= 366) return 52;
+  }
+
+  return weeks;
 }
 
 export function monthsBetween(start, end) {
   if (isUndefined(start) || isUndefined(end)) return 0;
 
-  return Math.ceil(
+  const months = Math.ceil(
     moment
       .utc(end)
       .endOf("month")
       .diff(moment.utc(start).startOf("month"), "month", true)
   );
+
+  if (months == 13) {
+    const days = Math.ceil(moment.utc(end).diff(moment.utc(start), "days", true));
+    if (days <= 366) return 12;
+  }
+
+  return months;
 }
