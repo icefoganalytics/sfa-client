6f2cb3c9c79ff3027b597f1a8c0ce796f7fd103b
Michael Johnson <michael@icefoganalytics.com>
2024-08-01 08:25:47 -0700

    Week calculation fix

--- src/api/utils/date-utils.ts
+++ src/api/utils/date-utils.ts
@@ -4,7 +4,9 @@ import moment from "moment";
 export function weeksBetween(start: Date | string | undefined, end: Date | string | undefined): number {
   if (isUndefined(start) || isUndefined(end)) return 0;
 
-  const weeks = Math.ceil(moment.utc(end).endOf("week").diff(moment.utc(start).startOf("week"), "week", false));
+  const weeks = Math.ceil(
+    moment.utc(end).endOf("week").add(1, "millisecond").diff(moment.utc(start).startOf("week"), "week", false)
+  );
 
   if (weeks == 53) {
     const days = Math.ceil(moment.utc(end).diff(moment.utc(start), "days", true));
@@ -17,7 +19,9 @@ export function weeksBetween(start: Date | string | undefined, end: Date | strin
 export function monthsBetween(start: Date | string | undefined, end: Date | string | undefined): number {
   if (isUndefined(start) || isUndefined(end)) return 0;
 
-  const months = Math.ceil(moment.utc(end).endOf("month").diff(moment.utc(start).startOf("month"), "month", true));
+  const months = Math.ceil(
+    moment.utc(end).endOf("month").add(1, "millisecond").diff(moment.utc(start).startOf("month"), "month", true)
+  );
 
   if (months == 13) {
     const days = Math.ceil(moment.utc(end).diff(moment.utc(start), "days", true));
